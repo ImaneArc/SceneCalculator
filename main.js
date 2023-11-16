be492840e9c4fc07a6c2040cc73fe69b18683555
ImaneArc <142788965+ImaneArc@users.noreply.github.com>
2023-11-16 19:26:17 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+
+window.onload = function bgImages (){
+    const body = document.body;
+    
+    const bodyImages = [
+        'image (1).jpg',
+        'image (2).jpg',
+        'image (3).jpg',
+        'image (4).jpg',
+        'image (5).jpg',
+        'image (6).jpg',
+        'image (7).jpg',
+        'image (8).jpg',
+        'image (9).jpg',
+        'image (9).jpg',
+        'image (10).jpg',
+        'image (11).jpg',
+        'image (12).jpg',
+        'image (13).jpg',
+        'image (14).jpg',
+    ];
+
+    const randomBg = Math.floor(Math.random() * bodyImages.length);
+
+    body.style.backgroundImage =`url('/images/${bodyImages[randomBg]}')`;
+}
+
+
+
+const display = document.getElementById('outputDisplay');
+const buttons = document.querySelectorAll('#buttons button');
+
+let output = '';
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => buttonClick( button.textContent));
+})
+
+function buttonClick (value){
+    
+            if(value==='=' || value=== 'Enter'){
+                try {
+                  output = eval(output);
+                  display.textContent = output;
+                }
+                catch(error){
+                    display.textContent ='Error';
+                }
+            } else if(value==='C'){
+                output ='';
+                display.textContent = '';
+            }
+            else if(value ==='Backspace' || value === 'X'){
+                output =output.slice(0,-1);
+                display.textContent = output;
+            }
+            else {
+             output += value;
+                display.textContent = output;
+            }
+
+}
+
+
+document.addEventListener('keydown', event => {
+    const key = event.key;
+
+    if (event.code.startsWith('F')) {
+    } else if (key.match(/[0-9+\-*/.=]|Enter|Backspace|Delete/i)) {
+        event.preventDefault();
+        buttonClick(key === 'Enter' ? '=' : key);
+    }
+});
